a7baf4cdac494ad931b9ff5637a1a4c416b09115
BScheihing <bruno.scheihing@ing.uchile.cl>
2015-12-08 11:05:04 -0300

    Procesa datos y crea graficos

--- bayes.py
+++ bayes.py
@@ -1,20 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+Este script modela la línea de absorción en el espectro de 'espectro.dat'
+usando técnicas bayesianas. Calcula y guarda las distribuciones marginales para
+los parámetros de dos modelos ajustados, la escala utilizada, las esperanzas
+asociadas a los parámetros y el factor bayesiano para determinar cuál modelo
+es mejor.
+'''
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def gaussian(x, mu, sigma):
+    '''
+    Define distribución gaussiana
+    '''
     return np.exp(-(x-mu)**2 / (2*sigma**2))/np.sqrt(2*np.pi*sigma**2)
 
+
 def prior1(params, prior_pars):
+    '''
+    Define densidad de probabilidad a priori para los parámetros en el modelo 1
+    (1 gaussiana)
+    '''
     Amp, std = params
     mu0, sigma0, mu1, sigma1 = prior_pars
     S = -1/2.0 * ((Amp - mu0)**2 / sigma0**2 + (std - mu1)**2 / sigma1**2)
     P = np.exp(S) / (2 * np.pi * sigma0 * sigma1)
     return P
 
+
 def prior2(params, prior_pars):
+    '''
+    Define densidad de probabilidad a priori para los parámetros en el modelo 2
+    (2 gaussianas)
+    '''
     Amp1, std1, Amp2, std2 = params
     mu0, sigma0, mu1, sigma1, mu2, sigma2, mu3, sigma3 = prior_pars
     S = -1/2.0 * ((Amp1 - mu0)**2 / sigma0**2 + (std1 - mu1)**2 / sigma1**2 +
@@ -22,40 +45,61 @@ def prior2(params, prior_pars):
     P = np.exp(S) / ((2*np.pi)**2 * sigma0 * sigma1 * sigma2 * sigma3)
     return P
 
+
 def fill_prior1(param0_grid, param1_grid, prior_pars):
+    '''
+    Calcula probabilidad a priori para los parámetros en el modelo 1
+    (1 gaussiana)
+    '''
     out = np.zeros(param0_grid.shape)
     ni, nj = param0_grid.shape
     for i in range(ni):
         for j in range(nj):
-            out[i, j] = prior1([param0_grid[i,j], param1_grid[i,j]], prior_pars)
+            out[i, j] = prior1([param0_grid[i, j], param1_grid[i, j]],
+                               prior_pars)
     return out
 
+
 def fill_prior2(par0_grid, par1_grid, par2_grid, par3_grid, prior_pars):
+    '''
+    Calcula probabilidad a priori para los parámetros en el modelo 2
+    (2 gaussianas)
+    '''
     out = np.zeros(par0_grid.shape)
     ni, nj, nk, nl = par0_grid.shape
     for i in range(ni):
         for j in range(nj):
             for k in range(nk):
                 for l in range(nl):
-                    out[i, j, k ,l] = prior2([par0_grid[i, j, k, l],
+                    out[i, j, k, l] = prior2([par0_grid[i, j, k, l],
                                               par1_grid[i, j, k, l],
                                               par2_grid[i, j, k, l],
                                               par3_grid[i, j, k, l]],
                                              prior_pars)
     return out
 
+
 def likelihood1(beta, data):
+    '''
+    Calcula similitud entre datos y el modelo 1 (1 gaussiana) para un set de
+    parámetros
+    '''
     beta0, beta1 = beta
     x, y = data
     try:
         N = len(x)
     except:
         N = 1
-    S = np.sum((y - lamb*100 + beta0 * gaussian(x, 6563, beta1) )**2)
+    S = np.sum((y - lamb*100 + beta0 * gaussian(x, 6563, beta1))**2)
     L = (2 * np.pi * noise**2)**(-N / 2.) * np.exp(-S / (2*noise**2))
     return L
 
+
 def likelihood2(beta, data):
+    '''
+    Calcula similitud entre datos y el modelo 2 (2 gaussianas) para un set de
+    parámetros
+    '''
     beta0, beta1, beta2, beta3 = beta
     x, y = data
     try:
@@ -63,19 +107,30 @@ def likelihood2(beta, data):
     except:
         N = 1
     S = np.sum((y - lamb*100 + beta0 * gaussian(x, 6563, beta1) +
-                beta2 * gaussian(x, 6563, beta3) )**2)
+                beta2 * gaussian(x, 6563, beta3))**2)
     L = (2 * np.pi * noise**2)**(-N / 2.) * np.exp(-S / (2*noise**2))
     return L
 
+
 def fill_likelihood1(beta0_grid, beta1_grid, data):
+    '''
+    Calcula similitud entre datos y el modelo 1 (1 gaussiana) para todo el
+    espacio de parámetros
+    '''
     output = np.zeros(beta0_grid.shape)
     ni, nj = beta0_grid.shape
     for i in range(ni):
         for j in range(nj):
-            output[i, j] = likelihood1([beta0_grid[i,j], beta1_grid[i,j]], data)
+            output[i, j] = likelihood1([beta0_grid[i, j], beta1_grid[i, j]],
+                                       data)
     return output
 
+
 def fill_likelihood2(beta0_grid, beta1_grid, beta2_grid, beta3_grid, data):
+    '''
+    Calcula similitud entre datos y el modelo 2 (2 gaussianas) para todo el
+    espacio de parámetros
+    '''
     output = np.zeros(beta0_grid.shape)
     ni, nj, nk, nl = beta0_grid.shape
     for i in range(ni):
@@ -89,8 +144,9 @@ def fill_likelihood2(beta0_grid, beta1_grid, beta2_grid, beta3_grid, data):
                                                      data)
     return output
 
+
 # Lectura de datos
-lamb = 0.4
+lamb = 2.45  # Parámetro para ajustar escala de los datos
 wavelen = np.loadtxt("espectro.dat", usecols=(0,))
 frec = np.loadtxt("espectro.dat", usecols=(1,))
 data = np.zeros((2, len(wavelen)))
@@ -101,7 +157,7 @@ data[1] = (lamb * 10**18)*frec
 s = 0
 n = 0
 for i in range(len(wavelen)):
-    if data[0][i] <= 6530 or data[0][i]>= 6600:
+    if data[0][i] <= 6530 or data[0][i] >= 6600:
         s += (data[1][i] - 100 * lamb)**2
         n += 1
 noise = np.sqrt(s/(1.0*n))
@@ -120,12 +176,19 @@ marg_Amp1 = np.sum(post_grid1, axis=1) * dy1 / P_E1
 marg_std1 = np.sum(post_grid1, axis=0) * dx1 / P_E1
 EAmp1 = np.sum(beta0_grid1[:, 0] * marg_Amp1) * dx1
 Estd1 = np.sum(beta1_grid1[0, :] * marg_std1) * dy1
+np.save('margAmp1.npy', marg_Amp1)
+np.save('dmargAmp1.npy', beta0_grid1[:, 0])
+np.save('margstd1.npy', marg_std1)
+np.save('dmargstd1.npy', beta1_grid1[0, :])
 
 # Modelo 2
-l = lamb
-beta0_grid2, beta1_grid2, beta2_grid2, beta3_grid2 = np.mgrid[8:24:101j,
+ai21 = 20 * lamb
+af21 = 60 * lamb
+ai22 = 35 * lamb
+af22 = 65 * lamb
+beta0_grid2, beta1_grid2, beta2_grid2, beta3_grid2 = np.mgrid[ai21:af21:101j,
                                                               1.0:4.0:101j,
-                                                              14:26:101j,
+                                                              ai22:af22:101j,
                                                               5.5:11.5:101j]
 beta_prior_pars2 = [40*lamb, 20*lamb, 2.5, 1, 50*lamb, 25*lamb, 8.5, 3]
 prior_grid2 = fill_prior2(beta0_grid2, beta1_grid2, beta2_grid2,
@@ -133,9 +196,9 @@ prior_grid2 = fill_prior2(beta0_grid2, beta1_grid2, beta2_grid2,
 likelihood_grid2 = fill_likelihood2(beta0_grid2, beta1_grid2, beta2_grid2,
                                     beta3_grid2, [data[0], data[1]])
 post_grid2 = likelihood_grid2 * prior_grid2
-dx2 = 16.0 / 100
+dx2 = (af21 - ai21) / 100
 dy2 = 3.0 / 100
-dz2 = 8.0 / 100
+dz2 = (af22 - ai22) / 100
 dw2 = 6.0 / 100
 P_E2 = np.sum(post_grid2) * dx2 * dy2 * dz2 * dw2
 marg_Amp21 = (np.sum(np.sum(np.sum(post_grid2, axis=1), axis=1), axis=1) *
@@ -151,25 +214,44 @@ Estd21 = np.sum(beta1_grid2[0, :, 0, 0] * marg_std21) * dy2
 EAmp22 = np.sum(beta2_grid2[0, 0, :, 0] * marg_Amp22) * dz2
 Estd22 = np.sum(beta3_grid2[0, 0, 0, :] * marg_std22) * dw2
 
+# Guarda resultados para procesar en otro script
+np.save('margAmp21.npy', marg_Amp21)
+np.save('dmargAmp21.npy', beta0_grid2[:, 0, 0, 0])
+np.save('margstd21.npy', marg_std21)
+np.save('dmargstd21.npy', beta1_grid2[0, :, 0, 0])
+np.save('margAmp22.npy', marg_Amp22)
+np.save('dmargAmp22.npy', beta2_grid2[0, 0, :, 0])
+np.save('margstd22.npy', marg_std22)
+np.save('dmargstd22.npy', beta3_grid2[0, 0, 0, :])
+np.save('scBayesianyE.npy', [lamb * 10**18, P_E1/P_E2, EAmp1, Estd1, EAmp21,
+                             Estd21, EAmp22, Estd22])
+
 print "Factor bayesiano:", P_E1/P_E2
+
+# Plots (solo referenciales, los resultados finales se procesan en otro script)
 
 plt.clf()
 # Plots modelo 1
 plt.figure(1)
 plt.pcolormesh(beta0_grid1, beta1_grid1, post_grid1)
+plt.title('Densidad de probabilidad en espacio de parametros, modelo 1')
 plt.figure(2)
 plt.plot(beta0_grid1[:, 0], marg_Amp1)
+plt.title('Densidad marginal de probabilidad para amplitud, modelo 1')
 plt.figure(3)
 plt.plot(beta1_grid1[0, :], marg_std1)
+plt.title('Densidad marginal de probabilidad para ancho, modelo 1')
 # Plots modelo 2
 plt.figure(4)
 plt.plot(beta0_grid2[:, 0, 0, 0], marg_Amp21)
+plt.title('Densidad marginal de probabilidad para amplitud 1, modelo 2')
 plt.figure(5)
 plt.plot(beta1_grid2[0, :, 0, 0], marg_std21)
+plt.title('Densidad marginal de probabilidad para ancho 1, modelo 2')
 plt.figure(6)
 plt.plot(beta2_grid2[0, 0, :, 0], marg_Amp22)
+plt.title('Densidad marginal de probabilidad para amplitud 2, modelo 2')
 plt.figure(7)
 plt.plot(beta3_grid2[0, 0, 0, :], marg_std22)
+plt.title('Densidad marginal de probabilidad para ancho 2, modelo 2')
 plt.show()
-
-# opt params: 0.4, 2.5, 0.5, 8.5
---
+++ processdat.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Este script presenta los resultados obtenidos a partir de bayes.py
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+wavelen = np.loadtxt("espectro.dat", usecols=(0,))
+frec = np.loadtxt("espectro.dat", usecols=(1,))
+E = np.load('scBayesianyE.npy')
+scala = E[0]
+bayes = E[1]
+A1 = np.load('margAmp1.npy')
+domA1 = np.load('dmargAmp1.npy')
+S1 = np.load('margstd1.npy')
+domS1 = np.load('dmargstd1.npy')
+A21 = np.load('margAmp21.npy')
+domA21 = np.load('dmargAmp21.npy')
+S21 = np.load('margstd21.npy')
+domS21 = np.load('dmargstd21.npy')
+A22 = np.load('margAmp22.npy')
+domA22 = np.load('dmargAmp22.npy')
+S22 = np.load('margstd22.npy')
+domS22 = np.load('dmargstd22.npy')
+doms = [domA1, domS1, domA21, domS21, domA22, domS22]
+margins = [A1, S1, A21, S21, A22, S22]
+emargins = [E[2], E[3], E[4], E[5], E[6], E[7]]
+cred_inter = []
+
+# Cálculo de intervalos de credibilidad
+for i in range(len(margins)):
+    dx = doms[i][1] - doms[i][0]
+    for n in range(len(doms[i])):
+        if doms[i][n] >= emargins[i]:
+            left = n - 1
+            right = n + 1
+            break
+    s = dx * margins[i][n]
+    while s < 0.68:
+        if margins[i][right] > margins[i][left]:
+            s += dx * margins[i][right]
+            right += 1
+        else:
+            s += dx * margins[i][left]
+            left -= 1
+    cred_inter.append((doms[i][left], doms[i][right]))
+
+print "Factor bayesiano P(D|M1) / P(D|M2) =", bayes
+print "Amplitud modelo 1:", emargins[0]/scala,
+print "Inter. de cred.:", cred_inter[0]
+print "Ancho modelo 1:", emargins[1], "Inter. de cred.:", cred_inter[1]
+print "Amplitud 1 modelo 2:", emargins[2]/scala,
+print "Inter. de cred.:", cred_inter[2]
+print "Ancho 1 modelo 2:", emargins[3], "Inter. de cred.:", cred_inter[3]
+print "Amplitud 2 modelo 2:", emargins[4], "Inter. de cred.:", cred_inter[4]
+print "Ancho 2 modelo 2:", emargins[5], "Inter. de cred.:", cred_inter[5]
+
+# Plots
+plt.clf()
+plt.figure(1)
+plt.plot(domA1/scala, A1*scala)
+plt.title('Densidad de probabilidad, amplitud, modelo 1')
+plt.xlabel('Amplitud [erg $s^{-1}$ $Hz^{-1}$ $cm^{-2}$]')
+plt.axvline(x=emargins[0]/scala, color='g')
+plt.axvline(x=cred_inter[0][0]/scala, color='r')
+plt.axvline(x=cred_inter[0][1]/scala, color='r')
+plt.savefig('A1.eps')
+
+plt.figure(2)
+plt.plot(domS1, S1)
+plt.title('Densidad de probabilidad, ancho de curva, modelo 1')
+plt.xlabel('$\sigma$ [$\AA$]')
+plt.axvline(x=emargins[1], color='g')
+plt.axvline(x=cred_inter[1][0], color='r')
+plt.axvline(x=cred_inter[1][1], color='r')
+plt.savefig('S1.eps')
+
+plt.figure(3)
+plt.plot(domA21/scala, A21*scala)
+plt.title('Densidad de probabilidad, amplitud 1, modelo 2')
+plt.xlabel('Amplitud [erg $s^{-1}$ $Hz^{-1}$ $cm^{-2}$]')
+plt.axvline(x=emargins[2]/scala, color='g')
+plt.axvline(x=cred_inter[2][0]/scala, color='r')
+plt.axvline(x=cred_inter[2][1]/scala, color='r')
+plt.savefig('A21.eps')
+
+plt.figure(4)
+plt.plot(domS21, S21)
+plt.title('Densidad de probabilidad, ancho de curva 1, modelo 2')
+plt.xlabel('$\sigma$ [$\AA$]')
+plt.axvline(x=emargins[3], color='g')
+plt.axvline(x=cred_inter[3][0], color='r')
+plt.axvline(x=cred_inter[3][1], color='r')
+plt.savefig('S21.eps')
+
+plt.figure(5)
+plt.plot(domA22/scala, A22*scala)
+plt.title('Densidad de probabilidad, amplitud 2, modelo 2')
+plt.xlabel('Amplitud [erg $s^{-1}$ $Hz^{-1}$ $cm^{-2}$]')
+plt.axvline(x=emargins[4]/scala, color='g')
+plt.axvline(x=cred_inter[4][0]/scala, color='r')
+plt.axvline(x=cred_inter[4][1]/scala, color='r')
+plt.savefig('A22.eps')
+
+plt.figure(6)
+plt.plot(domS22, S22)
+plt.title('Densidad de probabilidad, ancho de curva 2, modelo 2')
+plt.xlabel('$\sigma$ [$\AA$]')
+plt.axvline(x=emargins[5], color='g')
+plt.axvline(x=cred_inter[5][0], color='r')
+plt.axvline(x=cred_inter[5][1], color='r')
+plt.savefig('S22.eps')
