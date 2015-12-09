00111d28fdbf0a4fbc36d4cd36672e1a7028a414
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-12-08 23:37:30 -0400

    nuevos intentos con NUTS

--- parte1.py
+++ parte1.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats
 import pymc3 as pm
+from theano import function, shared
 import theano.tensor as tt
 
 
@@ -20,10 +21,20 @@ def leer_archivo(nombre):
 
 
 def gauss(mu, sigma, x):
-    y = tt.exp(- (x - mu) ** 2 / (2 * sigma ** 2))
-    return y
+    y = tt.exp(tt.as_tensor_variable(- (x - mu) ** 2 / (2 * sigma ** 2)))
+    f = function([], y)
+    return f()
 
 
+def modelo_doble(A1, sigma1, A2, sigma2, x_sample):
+    a = 10 ** (- 16)
+    mu = 6563
+    y = tt.as_tensor_variable(a - A1 * gauss(mu, sigma1, x_sample) - A2 * gauss(mu, sigma2, x_sample))
+    f = function([], y)
+    return f()
+
+
+'''
 def modelo_1(p, x):
     A, sigma = p
     a = 10 ** (- 16)
@@ -46,23 +57,14 @@ def gauss2d(x, y, mat_sigma, ):
     B = (- 1 / (2 * (2 - rho ** 2))) * ((x / sigma_x) ** 2 + (y / sigma_y) ** 2
                                         - 2 * rho * x * y / (sigma_x * sigma_y))
     return A * np.exp(B)
+'''
 
 
 # main
 x_sample, y_sample = leer_archivo('espectro.dat')
-'''
-fig = plt.figure()
-fig.clf()
-ax1 = fig.add_subplot(111)
-ax1.plot(x_sample, y_sample, '+', label="Espectro Experimental")
-ax1.plot()
-plt.legend(loc=4)
-plt.draw()
-plt.show()
-'''
 # inicializacion
 A1 = 1e-17
-A2 = 1e-17 / 2
+A2 = 1e-17
 sigma1 = 7.
 sigma2 = 7.
 a = 10 ** (- 16)
@@ -70,16 +72,16 @@ mu = 6563
 p = A1, sigma1, A2, sigma2
 with pm.Model() as basic_model:
     # priors
-    beta0 = pm.Normal('beta0', mu=A1, sd=10)
-    beta1 = pm.Normal('beta1', mu=sigma1, sd=10)
-    beta2 = pm.Normal('beta2', mu=A2, sd=10)
-    beta3 = pm.Normal('beta3', mu=sigma2, sd=10)
+    beta0 = pm.Normal('beta0', mu=A1, sd=1)
+    beta1 = pm.Normal('beta1', mu=sigma1, sd=1)
+    beta2 = pm.Normal('beta2', mu=A2, sd=1)
+    beta3 = pm.Normal('beta3', mu=sigma2, sd=1)
     # valor esperado
     p1 = beta0, beta1, beta2, beta3
     #y_out = modelo_2(p1, x_sample)
-    #y_out = a - A1 * scipy.stats.norm(loc=mu, scale=sigma1).pdf(x_sample)
-    y_out = a - beta0 * gauss(mu, beta1, x_sample) - beta2 * gauss(mu, beta3, x_sample)
-    #y_out = a - beta0 * scipy.stats.norm(loc=mu, scale=beta1).pdf(x_sample) - beta2 * scipy.stats.norm(loc=mu, scale=beta3).pdf(x_sample)
+    y_out = tt.as_tensor_variable(10 ** (- 16) - beta0 * tt.exp(tt.as_tensor_variable(- (x_sample - 6563) ** 2 / (2 * beta1 ** 2))) - beta2 * tt.exp(tt.as_tensor_variable(- (x_sample - 6563) ** 2 / (2 * beta3 ** 2))))
+    #y_out = modelo_doble(beta0, beta1, beta2, beta3, x_sample)
+    #y_out = a - beta0 * gauss(mu, beta1, x_sample) - beta2 * gauss(mu, beta3, x_sample)
     # likelihood
     Y_obs = pm.Normal('Y_obs', mu=y_out, sd=1.5, observed=y_sample)
 map_estimate = pm.find_MAP(model=basic_model)
