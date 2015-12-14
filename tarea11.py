c1f155a9aa7c17d57ed4f01aa549214075ffde5e
TatiFlores <tatiflores.4@gmail.com>
2015-12-13 23:03:33 -0300

    Funcionaaaa

--- tarea11.py
+++ tarea11.py
@@ -5,6 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats
 from scipy.optimize import curve_fit
+import pymc3 as pm
+import theano.tensor as T
+from scipy import optimize
 
 '''
 En este código se busca modelar el ensanchamiento de una linea de absorcion
@@ -13,8 +16,17 @@ Para ello se utilizan dos modelos: uno gaussiano simple y otro con dos
 funciones gaussianas.
 '''
 
-
 def modelo_1(x, A, sigma):
+    '''
+    Retorna la función correspondiente al primer modelo, es decir, una
+    gaussiana trasladada en 1e-16
+    A = amplitud ; s = sigma
+    '''
+    g = A * T.exp(-0.5 * ((x - 6563) / sigma)**2) / (sigma * np.sqrt(2*np.pi))
+    return 1e-16 - g
+
+
+def gauss_simple(x, A, sigma):
     '''
     Retorna la función correspondiente al primer modelo, es decir, una
     gaussiana trasladada en 1e-16
@@ -22,7 +34,20 @@ def modelo_1(x, A, sigma):
     return 1e-16 - A * scipy.stats.cauchy(loc=6563, scale=sigma).pdf(x)
 
 
-def modelo_2(x, Amplitud1, sigma1, Amplitud2, sigma2):
+
+def modelo_2(x, A1, s1, A2, s2):
+    '''
+    Retorna la función correspondiente al primer modelo, es decir, la suma
+    de dos gaussianas trasladada en 1e-16.
+    A = amplitud ; s = sigma
+    '''
+    mu = 6563
+    g1 = A1 * T.exp(-0.5 * ((x - mu) / s1)**2) / (s1 * np.sqrt(2*np.pi))
+    g2 = A2 * T.exp(-0.5 * ((x - mu) / s2)**2) / (s2 * np.sqrt(2*np.pi))
+    return 1e-16 - g1 - g2
+
+
+def gauss_doble(x, Amplitud1, sigma1, Amplitud2, sigma2):
     '''
     Retorna la función correspondiente al primer modelo, es decir, la suma
     de dos gaussianas trasladada en 1e-16
@@ -30,81 +55,6 @@ def modelo_2(x, Amplitud1, sigma1, Amplitud2, sigma2):
     gauss1 = (Amplitud1 * scipy.stats.norm(loc=6563, scale=sigma1).pdf(x))
     gauss2 = (Amplitud2 * scipy.stats.norm(loc=6563, scale=sigma2).pdf(x))
     return 1e-16 - gauss1 - gauss2
-
-
-def make_figure_axes(x, y, fig_number=1, fig_size=8):
-    '''
-    Creates a set of 3 axes to plot 2D function + marginals
-    '''
-    # determine max size
-    size_x = x.max() - x.min()
-    size_y = y.max() - y.min()
-    max_size = max(size_x, size_y)
-    min_size = min(size_x, size_y)
-
-    if size_x >= size_y:
-        fig_size_x = fig_size
-        fig_size_y = (0.12 * fig_size_x +
-                      0.65 * fig_size_x * min_size / max_size +
-                      0.02 * fig_size_x +
-                      0.18 * fig_size_x +
-                      0.03 * fig_size_x)
-        rect_main = [0.12,
-                     0.12 * fig_size_x / fig_size_y,
-                     0.65,
-                     0.65 * fig_size_x * min_size / max_size / fig_size_y]
-        rect_x = [0.12, ((0.12 + 0.65 * min_size / max_size + 0.02) *
-                         fig_size_x / fig_size_y),
-                  0.65, 0.18 * fig_size_x / fig_size_y]
-        rect_y = [0.79, 0.12 * fig_size_x / fig_size_y,
-                  0.18, 0.65 * fig_size_x * min_size / max_size / fig_size_y]
-
-    else:
-        fig_size_y = fig_size
-        fig_size_x = (0.12 * fig_size_y +
-                      0.65 * fig_size_y * min_size / max_size +
-                      0.02 * fig_size_y +
-                      0.18 * fig_size_y +
-                      0.03 * fig_size_y)
-        rect_main = [0.12 * fig_size_y / fig_size_x,
-                     0.12,
-                     0.65 * fig_size_y * min_size / max_size / fig_size_x,
-                     0.65]
-        rect_x = [0.12 * fig_size_y / fig_size_x, 0.79,
-                  0.65 * fig_size_y * min_size / max_size / fig_size_x, 0.18]
-        rect_y = [((0.12 + 0.65 * min_size / max_size + 0.02) *
-                   fig_size_y / fig_size_x), 0.12,
-                  0.18 * fig_size_y / fig_size_x, 0.65]
-
-    fig = plt.figure(fig_number, figsize=(fig_size_x, fig_size_y))
-    fig.clf()
-    ax_main = fig.add_axes(rect_main)
-    ax_marginal_x = fig.add_axes(rect_x, xticklabels=[])
-    ax_marginal_y = fig.add_axes(rect_y, yticklabels=[])
-
-    return ax_main, ax_marginal_x, ax_marginal_y
-
-
-def plot_distribution(x, y, z, cmap='PuBu_r'):
-    x_limits = (x.min(), x.max())
-    y_limits = (y.min(), y.max())
-
-    ax_main, ax_marginal_x, ax_marginal_y = make_figure_axes(x, y)
-    ax_main.pcolormesh(x, y, z, cmap=cmap)
-
-    marginal_x = np.sum(z, axis=1)
-    ax_marginal_x.plot(x[:, 0], marginal_x)
-    [l.set_rotation(-90) for l in ax_marginal_y.get_xticklabels()]
-
-    marginal_y = np.sum(z, axis=0)
-    ax_marginal_y.plot(marginal_y, y[0])
-
-    ax_main.set_xlim(x_limits)
-    ax_main.set_ylim(y_limits)
-
-    ax_marginal_x.set_xlim(x_limits)
-    ax_marginal_y.set_ylim(y_limits)
-    return ax_main, ax_marginal_x, ax_marginal_y
 
 
 def likelihood(data, params, modelo):
@@ -183,99 +133,232 @@ def fill_prior_2(beta0_grid, beta1_grid, beta2_grid, beta3_grid, prior_params,
 # Main
 # Datos
 wavelength = np.loadtxt("espectro.dat", usecols=[0])
-Fnu = np.loadtxt("espectro.dat", usecols=[1])
+Fnu = np.loadtxt("espectro.dat", usecols=[1]) * 1e+16
 x = np.linspace(min(wavelength), max(wavelength), 100)
+N = 5000
 
-# Modelo 1:
-Amplitud_mod1 = Fnu.max() - Fnu.min()
-sigma_mod1 = 6  # Del gráfico
-adivinanza_mod1 = [Amplitud_mod1, 0.1e-16, sigma_mod1, 2]
+with pm.Model() as basic_model1:
 
-beta0_grid, beta1_grid = np.mgrid[-0.0001:0.0001:201j, -5:15:201j]
-n0, n1 = beta0_grid.shape
-prior_m1 = np.zeros((n0, n1))
-likelihood_m1 = np.zeros((n0, n1))
-prior_m1, likelihood_m1 = fill_prior_1(beta0_grid, beta1_grid, adivinanza_mod1,
-                                       [wavelength, Fnu])
+    # Priors for unknown model parameters
+    Amplitud_mod1 = pm.Normal('Amplitud_mod1', mu=0.917, sd=0.08)
+    sigma_mod1 = pm.Normal('sigma_mod1', mu=3.7, sd=0.5)
 
-post_grid1 = likelihood_m1 * prior_m1
-dx1 = 0.0002 / 200
-dy1 = 20 / 200
-P_E1 = np.sum(post_grid1) * dx1 * dy1
-marg_Amplitud_mod1 = np.sum(post_grid1, axis=1) * dy1 / P_E1
-marg_sigma_mod1 = np.sum(post_grid1, axis=0) * dx1 / P_E1
-E_Amplitud_mod1 = np.sum(beta0_grid[:, 0] * marg_Amplitud_mod1) * dx1
-E_sigma_mod1 = np.sum(beta1_grid[0, :] * marg_sigma_mod1) * dy1
+    # Expected value of outcome
+    y_out = modelo_1 (wavelength, Amplitud_mod1, sigma_mod1)
 
+    # Likelihood (sampling distribution) of observations
+    Y_obs = pm.Normal('Y_obs', mu=y_out, sd=1.5, observed=Fnu)
+
+
+map_estimate = pm.find_MAP(model=basic_model1, fmin=optimize.fmin_powell)
+print(map_estimate)
+with basic_model1:
+    trace1 = pm.sample(N, start=map_estimate)
+
+
+# Resultados
+E_Amplitud_mod1 = np.mean(trace1.Amplitud_mod1) * 1e-16
+E_sigma_mod1 = np.mean(trace1.sigma_mod1)
+
+Amplitud_mod1_sort = np.sort(trace1.Amplitud_mod1)
+sigma_mod1_sort = np.sort(trace1.sigma_mod1)
+
+A_limite_bajo = Amplitud_mod1_sort[int(N * 0.16)]
+A_limite_alto = Amplitud_mod1_sort[int(N * 0.84)]
+s_limite_bajo = sigma_mod1_sort[int(N * 0.16)]
+s_limite_alto = sigma_mod1_sort[int(N * 0.84)]
+
+print ''
 print 'Primer modelo: Gaussiana simple'
-print 'Amplitud                :', E_Amplitud_mod1
-print 'sigma                   :', E_sigma_mod1
+print 'Amplitud                        :', E_Amplitud_mod1
+print 'Intervalo de credibilidad al 68%:', [A_limite_bajo, A_limite_alto]
+print 'sigma                           :', E_sigma_mod1
+print 'Intervalo de credibilidad al 68%:', [s_limite_bajo, s_limite_alto]
 print ''
 
 
-# Modelo 2:
-Amplitud1_mod2 = 0.02e-16
-sigma1_mod2 = 6.
-Amplitud2_mod2 = 0.07e-16
-sigma2_mod2 = 1.5
-adivinanza_mod2 = [Amplitud1_mod2, 2, sigma1_mod2, 2, Amplitud2_mod2, 2,
-                   sigma2_mod2, 2]
-beta0_grid, beta1_grid, beta2_grid, beta3_grid = np.mgrid[-0.5:0.5:51j,
-                                                          -5:15:51j,
-                                                          -0.5:0.5:51j,
-                                                          -5:15:51j]
-prior_m2, likelihood_m2 = fill_prior_2(beta0_grid, beta1_grid, beta2_grid,
-                                       beta3_grid, adivinanza_mod2,
-                                       [wavelength, Fnu])
+with pm.Model() as basic_model2:
+    # Priors for unknown model parameters
+    Amplitud1_mod2 = pm.Normal('Amplitud1_mod2', mu=0.6, sd=0.05)
+    sigma1_mod2 = pm.Normal('sigma1_mod2', mu=7, sd=1.5)
+    Amplitud2_mod2 = pm.Normal('Amplitud2_mod2', mu=0.2, sd=0.05)
+    sigma2_mod2 = pm.Normal('sigma2_mod2', mu=1.5, sd=1.5)
 
-post_grid2 = likelihood_m2 * prior_m2
-dx2 = 1 / 200
-dy2 = 20 / 200
-dz2 = 1 / 200
-dt2 = 20 / 200
-P_E2 = np.sum(post_grid2) * dx2 * dy2 * dz2 * dt2
-marg_Amplitud1_mod2 = (np.sum(np.sum(np.sum(post_grid2, axis=1), axis=1),
-                              axis=1) * dy2 * dz2 * dt2 / P_E2)
-marg_sigma1_mod2 = (np.sum(np.sum(np.sum(post_grid2, axis=1), axis=1),
-                              axis=1) * dx2 * dz2 * dt2 / P_E2)
-marg_Amplitud2_mod2 = (np.sum(np.sum(np.sum(post_grid2, axis=1), axis=1),
-                              axis=1) * dx2 * dy2 * dt2 / P_E2)
-marg_sigma2_mod2 = (np.sum(np.sum(np.sum(post_grid2, axis=1), axis=1),
-                              axis=1) * dx2 * dy2 * dz2 / P_E2)
+    # Expected value of outcome
+    y_out = modelo_2(wavelength, Amplitud1_mod2, sigma1_mod2, Amplitud2_mod2, sigma2_mod2)
 
-E_Amplitud1_mod2 = np.sum(beta0_grid[:, 0] * marg_Amplitud1_mod2) * dx2
-E_sigma1_mod2 = np.sum(beta1_grid[0, :] * marg_sigma1_mod2) * dy2
-E_Amplitud2_mod2 = np.sum(beta0_grid[:, 0] * marg_Amplitud2_mod2) * dz2
-E_sigma2_mod2 = np.sum(beta1_grid[0, :] * marg_sigma2_mod2) * dt2
+    # Likelihood (sampling distribution) of observations
+    Y_obs = pm.Normal('Y_obs', mu=y_out, sd=1.5, observed=Fnu)
 
+map_estimate = pm.find_MAP(model=basic_model2, fmin=optimize.fmin_powell)
+print(map_estimate)
+with basic_model2:
+    trace2 = pm.sample(N, start=map_estimate)
+
+# Resultados
+E_Amplitud1_mod2 = np.mean(trace2.Amplitud1_mod2) * 1e-16
+E_sigma1_mod2 = np.mean(trace2.sigma1_mod2)
+E_Amplitud2_mod2 = np.mean(trace2.Amplitud2_mod2) * 1e-16
+E_sigma2_mod2 = np.mean(trace2.sigma2_mod2)
+
+Amplitud1_mod2_sort = np.sort(trace2.Amplitud1_mod2)
+sigma1_mod2_sort = np.sort(trace2.sigma1_mod2)
+Amplitud2_mod2_sort = np.sort(trace2.Amplitud2_mod2)
+sigma2_mod2_sort = np.sort(trace2.sigma2_mod2)
+
+A1_limite_bajo = Amplitud1_mod2_sort[int(N * 0.16)]
+A1_limite_alto = Amplitud1_mod2_sort[int(N * 0.84)]
+s1_limite_bajo = sigma1_mod2_sort[int(N * 0.16)]
+s1_limite_alto = sigma1_mod2_sort[int(N * 0.84)]
+A2_limite_bajo = Amplitud2_mod2_sort[int(N * 0.16)]
+A2_limite_alto = Amplitud2_mod2_sort[int(N * 0.84)]
+s2_limite_bajo = sigma2_mod2_sort[int(N * 0.16)]
+s2_limite_alto = sigma2_mod2_sort[int(N * 0.84)]
+
+print ''
 print 'Segundo modelo: Gaussiana doble'
-print 'Amplitud 1              :', E_Amplitud1_mod2
-print 'sigma 1                 :', E_sigma1_mod2
-print 'Amplitud 2              :', E_Amplitud2_mod2
-print 'sigma 2                 :', E_sigma2_mod2
+print 'Amplitud 1                      :', E_Amplitud1_mod2
+print 'Intervalo de credibilidad al 68%:', [A1_limite_bajo, A1_limite_alto]
+print 'sigma 1                         :', E_sigma2_mod2
+print 'Intervalo de credibilidad al 68%:', [s1_limite_bajo, s1_limite_alto]
+
+print 'Amplitud 2                      :', E_Amplitud2_mod2
+print 'Intervalo de credibilidad al 68%:', [A2_limite_bajo, A2_limite_alto]
+print 'sigma 2                         :', E_sigma2_mod2
+print 'Intervalo de credibilidad al 68%:', [s2_limite_bajo, s2_limite_alto]
 print ''
 
 
-# Plots
-
-plt.figure(1)
+plt.figure(1, figsize=(10, 7))
 plt.style.use('bmh')
-plt.rcParams['xtick.labelsize'] = 'large'
-plt.rcParams['ytick.labelsize'] = 'large'
-plot_distribution(beta0_grid, beta1_grid, prior_m1 * likelihood_m1)
-plt.savefig('fig_1.eps')
+plt.plot(wavelength, Fnu*1e-16, color='brown', drawstyle='steps-post',
+         label='Datos')
+plt.plot(x, gauss_simple(x, E_Amplitud_mod1, E_sigma_mod1), label='Modelo 1 (Gaussiana simple)',
+         linewidth=2.0)
+plt.xlim(6520, 6600)
+plt.legend(loc='lower left')
+plt.savefig('Fit_mod1.eps')
 
-plt.figure(2)
+
+plt.figure(2, figsize=(10, 7))
 plt.style.use('bmh')
-plt.rcParams['xtick.labelsize'] = 'large'
-plt.rcParams['ytick.labelsize'] = 'large'
-plt.plot(beta0_grid, marg_Amplitud_mod1)
-plt.savefig('Densidad_prob_Amod1.eps')
+plt.plot(wavelength, Fnu*1e-16, color='brown', drawstyle='steps-post',
+         label='Datos')
+plt.plot(x, gauss_doble(x, E_Amplitud1_mod2, E_sigma1_mod2, E_Amplitud2_mod2, E_sigma2_mod2), label='Modelo 2 (Gaussiana doble)',
+         linewidth=2.0)
+plt.plot(x, gauss_simple(x, E_Amplitud1_mod2, E_sigma1_mod2), '--', color='g',
+         label='Gaussiana 1 (modelo 2)', alpha=0.8)
+plt.plot(x, gauss_simple(x, E_Amplitud2_mod2, E_sigma2_mod2), '-.', color='g',
+         label='Gaussiana 2 (modelo 2)', alpha=0.8)
+plt.xlim(6520, 6600)
+plt.legend(loc='lower left')
+plt.savefig('Fit.mod2.eps')
 
 plt.figure(3)
-plt.style.use('bmh')
-plt.rcParams['xtick.labelsize'] = 'large'
-plt.rcParams['ytick.labelsize'] = 'large'
-plt.plot(beta1_grid, marg_sigma_mod1)
-plt.savefig('Densidad_prob_sigmamod1.eps')
+pm.traceplot(trace2)
+plt.savefig('plot_diagnostico_mod2')
+
+plt.figure(4)
+[[ax11, ax12], [ax21, ax22]] = pm.traceplot(trace1)
+ax11.axvline(0.917)
+ax21.axvline(3.7)
+plt.savefig('plot_diagnostico_mod1')
+
 plt.show()
+
+
+
+
+# # Modelo 1:
+# Amplitud_mod1 = Fnu.max() - Fnu.min()
+# sigma_mod1 = 6  # Del gráfico
+# adivinanza_mod1 = [Amplitud_mod1, 0.1e-16, sigma_mod1, 2]
+#
+# beta0_grid, beta1_grid = np.mgrid[-0.0001:0.0001:201j, -5:15:201j]
+# n0, n1 = beta0_grid.shape
+# prior_m1 = np.zeros((n0, n1))
+# likelihood_m1 = np.zeros((n0, n1))
+# prior_m1, likelihood_m1 = fill_prior_1(beta0_grid, beta1_grid, adivinanza_mod1,
+#                                        [wavelength, Fnu])
+#
+# post_grid1 = likelihood_m1 * prior_m1
+# dx1 = 0.0002 / 200
+# dy1 = 20 / 200
+# P_E1 = np.sum(post_grid1) * dx1 * dy1
+# marg_Amplitud_mod1 = np.sum(post_grid1, axis=1) * dy1 / P_E1
+# marg_sigma_mod1 = np.sum(post_grid1, axis=0) * dx1 / P_E1
+# E_Amplitud_mod1 = np.sum(beta0_grid[:, 0] * marg_Amplitud_mod1) * dx1
+# E_sigma_mod1 = np.sum(beta1_grid[0, :] * marg_sigma_mod1) * dy1
+#
+# print 'Primer modelo: Gaussiana simple'
+# print 'Amplitud                :', E_Amplitud_mod1
+# print 'sigma                   :', E_sigma_mod1
+# print ''
+#
+#
+# # Modelo 2:
+# Amplitud1_mod2 = 0.02e-16
+# sigma1_mod2 = 6.
+# Amplitud2_mod2 = 0.07e-16
+# sigma2_mod2 = 1.5
+# adivinanza_mod2 = [Amplitud1_mod2, 2, sigma1_mod2, 2, Amplitud2_mod2, 2,
+#                    sigma2_mod2, 2]
+# beta0_grid, beta1_grid, beta2_grid, beta3_grid = np.mgrid[-0.5:0.5:51j,
+#                                                           -5:15:51j,
+#                                                           -0.5:0.5:51j,
+#                                                           -5:15:51j]
+# prior_m2, likelihood_m2 = fill_prior_2(beta0_grid, beta1_grid, beta2_grid,
+#                                        beta3_grid, adivinanza_mod2,
+#                                        [wavelength, Fnu])
+#
+# post_grid2 = likelihood_m2 * prior_m2
+# dx2 = 1 / 200
+# dy2 = 20 / 200
+# dz2 = 1 / 200
+# dt2 = 20 / 200
+# P_E2 = np.sum(post_grid2) * dx2 * dy2 * dz2 * dt2
+# marg_Amplitud1_mod2 = (np.sum(np.sum(np.sum(post_grid2, axis=1), axis=1),
+#                               axis=1) * dy2 * dz2 * dt2 / P_E2)
+# marg_sigma1_mod2 = (np.sum(np.sum(np.sum(post_grid2, axis=1), axis=1),
+#                               axis=1) * dx2 * dz2 * dt2 / P_E2)
+# marg_Amplitud2_mod2 = (np.sum(np.sum(np.sum(post_grid2, axis=1), axis=1),
+#                               axis=1) * dx2 * dy2 * dt2 / P_E2)
+# marg_sigma2_mod2 = (np.sum(np.sum(np.sum(post_grid2, axis=1), axis=1),
+#                               axis=1) * dx2 * dy2 * dz2 / P_E2)
+#
+# E_Amplitud1_mod2 = np.sum(beta0_grid[:, 0] * marg_Amplitud1_mod2) * dx2
+# E_sigma1_mod2 = np.sum(beta1_grid[0, :] * marg_sigma1_mod2) * dy2
+# E_Amplitud2_mod2 = np.sum(beta0_grid[:, 0] * marg_Amplitud2_mod2) * dz2
+# E_sigma2_mod2 = np.sum(beta1_grid[0, :] * marg_sigma2_mod2) * dt2
+#
+# print 'Segundo modelo: Gaussiana doble'
+# print 'Amplitud 1              :', E_Amplitud1_mod2
+# print 'sigma 1                 :', E_sigma1_mod2
+# print 'Amplitud 2              :', E_Amplitud2_mod2
+# print 'sigma 2                 :', E_sigma2_mod2
+# print ''
+#
+#
+# # Plots
+#
+# plt.figure(1)
+# plt.style.use('bmh')
+# plt.rcParams['xtick.labelsize'] = 'large'
+# plt.rcParams['ytick.labelsize'] = 'large'
+# plot_distribution(beta0_grid, beta1_grid, prior_m1 * likelihood_m1)
+# plt.savefig('fig_1.eps')
+#
+# plt.figure(2)
+# plt.style.use('bmh')
+# plt.rcParams['xtick.labelsize'] = 'large'
+# plt.rcParams['ytick.labelsize'] = 'large'
+# plt.plot(beta0_grid, marg_Amplitud_mod1)
+# plt.savefig('Densidad_prob_Amod1.eps')
+#
+# plt.figure(3)
+# plt.style.use('bmh')
+# plt.rcParams['xtick.labelsize'] = 'large'
+# plt.rcParams['ytick.labelsize'] = 'large'
+# plt.plot(beta1_grid, marg_sigma_mod1)
+# plt.savefig('Densidad_prob_sigmamod1.eps')
+# plt.show()
