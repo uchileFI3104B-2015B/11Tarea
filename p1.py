3ef5ea581e98240855f848beca262c46cb0df537
hpozojose <hpozo.jose@gmail.com>
2015-12-13 23:10:46 -0300

    luego de trabajar mucho en el la obtencion de nan me rindo y agrego informe sin resultados :c Hoy es un dia triste

--- p1.py
+++ p1.py
@@ -11,89 +11,12 @@ from IPython.display import Image
 
 
 '''
-Este script modela 2 parametros a la vez a partir de los datos de
+Este script modela 2 y 4 parametros a la vez a partir de los datos de
 espectro.dat, al plotear se observa un segmento del espectro de una fuente
 que muesta continuo y una linea de absorcion, el continuo esta dado y la linea
-de absorcion se modela con una gaussiana
+de absorcion se modela con una o doble gaussiana
 '''
-plt.style.use('bmh')
-plt.rcParams['xtick.labelsize'] = 'large'
-plt.rcParams['ytick.labelsize'] = 'large'
-plt.rcParams['figure.figsize'] = '7, 5'
 
-def make_figure_axes(x, y, fig_number=1, fig_size=8):
-    '''
-    Crea un set de 3 ejes para plotear un grafico 2D funcion + marginals
-    '''
-    # determine max size
-    size_x = x.max() - x.min()
-    size_y = y.max() - y.min()
-    max_size = max(size_x, size_y)
-    min_size = min(size_x, size_y)
-
-    if size_x >= size_y:
-        fig_size_x = fig_size
-        fig_size_y = (0.12 * fig_size_x +
-                      0.65 * fig_size_x * min_size / max_size +
-                      0.02 * fig_size_x +
-                      0.18 * fig_size_x +
-                      0.03 * fig_size_x)
-        rect_main = [0.12,
-                     0.12 * fig_size_x / fig_size_y,
-                     0.65,
-                     0.65 * fig_size_x * min_size / max_size / fig_size_y]
-        rect_x = [0.12, ((0.12 + 0.65 * min_size / max_size + 0.02) *
-                         fig_size_x / fig_size_y),
-                  0.65, 0.18 * fig_size_x / fig_size_y]
-        rect_y = [0.79, 0.12 * fig_size_x / fig_size_y,
-                  0.18, 0.65 * fig_size_x * min_size / max_size / fig_size_y]
-    else:
-        fig_size_y = fig_size
-        fig_size_x = (0.12 * fig_size_y +
-                      0.65 * fig_size_y * min_size / max_size +
-                      0.02 * fig_size_y +
-                      0.18 * fig_size_y +
-                      0.03 * fig_size_y)
-        rect_main = [0.12 * fig_size_y / fig_size_x,
-                     0.12,
-                     0.65 * fig_size_y * min_size / max_size / fig_size_x,
-                     0.65]
-        rect_x = [0.12 * fig_size_y / fig_size_x, 0.79,
-                  0.65 * fig_size_y * min_size / max_size / fig_size_x, 0.18]
-        rect_y = [((0.12 + 0.65 * min_size / max_size + 0.02) *
-                   fig_size_y / fig_size_x), 0.12,
-                  0.18 * fig_size_y / fig_size_x, 0.65]
-
-    fig = plt.figure(fig_number, figsize=(fig_size_x, fig_size_y))
-    fig.clf()
-
-    ax_main = fig.add_axes(rect_main)
-    ax_marginal_x = fig.add_axes(rect_x, xticklabels=[])
-    ax_marginal_y = fig.add_axes(rect_y, yticklabels=[])
-
-    return ax_main, ax_marginal_x, ax_marginal_y
-
-
-def plot_distribution(x, y, z, cmap='PuBu_r'):
-    x_limits = (x.min(), x.max())
-    y_limits = (y.min(), y.max())
-
-    ax_main, ax_marginal_x, ax_marginal_y = make_figure_axes(x, y)
-    ax_main.pcolormesh(x, y, z, cmap=cmap)
-
-    marginal_x = np.sum(z, axis=1)
-    ax_marginal_x.plot(x[:,0], marginal_x)
-    [l.set_rotation(-90) for l in ax_marginal_y.get_xticklabels()]
-
-    marginal_y = np.sum(z, axis=0)
-    ax_marginal_y.plot(marginal_y, y[0])
-
-    ax_main.set_xlim(x_limits)
-    ax_main.set_ylim(y_limits)
-
-    ax_marginal_x.set_xlim(x_limits)
-    ax_marginal_y.set_ylim(y_limits)
-    return ax_main, ax_marginal_x, ax_marginal_y
 
 def datos():
     '''
@@ -103,12 +26,19 @@ def datos():
     flujo = np.loadtxt('espectro.dat', usecols=(-1,))
     return lambda1, flujo
 
-
 def gaussiana(x, A, sigma):
     '''
     modela linea de absorcion a traves de una gaussiana
     '''
-    return 1e-16 - A * scipy.stats.norm(loc=6563, scale=sigma).pdf(x)
+    return 1e-16 - (A * scipy.stats.norm(loc=6563, scale=sigma).pdf(x)) * 0.08
+
+
+def gaussiana2(x, A, A2, sigma, sigma2):
+    '''
+    modela linea de absorsion a traves de doble gaussiana
+    '''
+    return 1e-16 - (A * scipy.stats.cauchy(loc=6563, scale=sigma).pdf(x) +
+            A2 * scipy.stats.cauchy(loc=6563, scale=sigma2).pdf(x))
 
 
 def prior(beta, params):
@@ -119,20 +49,21 @@ def prior(beta, params):
     if len(beta)==2:
         beta0, beta1 = beta
         mu0, sigma0, mu1, sigma1 = params
-        S = -1. / 2 * ((beta0 - mu0)**2 / sigma0**2 + (beta1 - mu1)**2 / sigma1**2)
+        S = -1. / 2 * ((beta0 - mu0)**2 / sigma0**2 +
+                       (beta1 - mu1)**2 / sigma1**2)
         P = np.exp(S) / (2 * np.pi * sigma0 * sigma1)
-    elif(beta)==4
+    elif len(beta)==4:
         beta0, beta1, beta2, beta3 = beta
         mu0, sigma0, mu1, sigma1, mu2, sigma2, mu3, sigma3 = params
         S0 = -1. / 2 * (beta0 - mu0)**2 / sigma0**2
         S1 = -1. / 2 * (beta1 - mu1)**2 / sigma1**2
         S2 = -1. / 2 * (beta2 - mu2)**2 / sigma2**2
         S3 = -1. / 2 * (beta3 - mu3)**2 / sigma3**2
-        P = np.exp(S0 + S1 + S2 + S3)
-        P = P / (2 * np.pi * sigma0 * sigma1 * sigma2 * sigma3)
+        SS = np.exp(S0 + S1 + S2 + S3)
+        P = SS / (2 * np.pi * sigma0 * sigma1 * sigma2 * sigma3)
     return P
 
-def fill_prior(beta0_grid, beta1_grid, prior_params):
+def fill_prior_1(beta0_grid, beta1_grid, prior_params):
     output = np.zeros(beta0_grid.shape)
     ni, nj = beta0_grid.shape
     for i in range(ni):
@@ -140,11 +71,26 @@ def fill_prior(beta0_grid, beta1_grid, prior_params):
             output[i, j] = prior([beta0_grid[i,j], beta1_grid[i,j]], prior_params)
     return output
 
+
+def fill_prior_2(beta0_grid, beta1_grid, beta2_grid, beta3_grid, prior_params,
+                 data):
+    output = np.zeros(beta0_grid.shape)
+    ni, nj, nk, nl = beta0_grid.shape
+    for i in range(ni):
+        for j in range(nj):
+            for k in range(nk):
+                for l in range(nl):
+                    output[i, j, k, l] = prior([beta0_grid[i, j, k, l],
+                                               beta1_grid[i, j, k, l],
+                                               beta2_grid[i, j, k, l],
+                                               beta3_grid[i, j, k, l]],
+                                               prior_params)
+    return output
+
 def likelihood(beta, data, modelo):
     '''
     Calcula verosimilitud
     '''
-    beta0, beta1 = beta
     x, y = data
     try:
         N = len(x)
@@ -154,13 +100,16 @@ def likelihood(beta, data, modelo):
     L = (2 * np.pi * 1.5**2)**(-N / 2.) * np.exp(-S / 2 / 1.5**2)
     return L
 
-def fill_likelihood(beta0_grid, beta1_grid, data):
+def fill_likelihood_m1(beta0_grid, beta1_grid, data):
     output = np.zeros(beta0_grid.shape)
     ni, nj = beta0_grid.shape
     for i in range(ni):
         for j in range(nj):
-            output[i, j] = likelihood([beta0_grid[i,j], beta1_grid[i,j]], data)
+            output[i, j] = likelihood([beta0_grid[i,j], beta1_grid[i,j]],
+                                      data, gaussiana)
     return output
+
+
 
 # Main
 
@@ -170,11 +119,27 @@ x = np.linspace(min(x_sample), max(x_sample))
 
 # Grilla suficientemente grande
 beta0_grid, beta1_grid = np.mgrid[-0.0001:0.0001:201j, -5:15:201j]
-dx = 0.0002 / 200
-dy = 20 / 100
+dx1 = 0.0002 / 200
+dy1 = 20 / 200
 
-prior_params = [0.08e-16, 100, 3, 100]
-prior_grid = fill_prior(beta0_grid, beta1_grid, prior_params)
-likelihood_grid = fill_likelihood(beta0_grid, beta1_grid, [x_sample, y_sample])
+# Modelo 1
+prior_params = [0.02e-16, 10, 3, 10]
+prior_grid_m1 = fill_prior_1(beta0_grid, beta1_grid, prior_params)
+likelihood_grid_m1 = fill_likelihood_m1(beta0_grid, beta1_grid, [x_sample, y_sample])
 
-ax_main, ax_0, ax_1 = plot_distribution(beta0_grid, beta1_grid, likelihood_grid * prior_grid)
+post_grid1 = likelihood_grid_m1 * prior_grid_m1
+P_E1 = np.sum(post_grid1) * dx1 * dy1
+marg_A_mod1 = np.sum(post_grid1, axis=1) * dy1 / P_E1
+marg_sigma_mod1 = np.sum(post_grid1, axis=0) * dx1 / P_E1
+
+# Calculo parametros
+E_A_mod1 = np.sum(beta0_grid[:, 0] * marg_A_mod1) * dx1
+E_sigma_mod1 = np.sum(beta1_grid[0, :] * marg_sigma_mod1) * dy1
+
+print 'Primer modelo: Gaussiana simple'
+print 'Amplitud                :', E_A_mod1
+print 'sigma                   :', E_sigma_mod1
+print ''
+
+# Modelo 2
+prior_params2 = [0.02e-16, 0.04e-16, 6, 3]
