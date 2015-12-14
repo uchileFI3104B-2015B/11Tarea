283c0be164978d9579f244c3ad86aebbb8fbfda9
Martín!!! <martin_panza_p@hotmail.com>
2015-12-13 23:22:34 -0300

    modificaciones y pep8

--- Parte1.py
+++ Parte1.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec 10 22:54:07 2015
-
-@author: splatt
+Se modela una línea de absorción de una observación espectroscópica
+utilizando técnicas Bayesianas
 """
 
 from __future__ import division
@@ -13,6 +12,7 @@ plt.style.use('bmh')
 plt.rcParams['xtick.labelsize'] = 'large'
 plt.rcParams['ytick.labelsize'] = 'large'
 plt.rcParams['figure.figsize'] = '7, 5'
+
 
 def make_figure_axes(x, y, fig_number=1, fig_size=8):
     '''
@@ -75,7 +75,7 @@ def plot_distribution(x, y, z, cmap='viridis'):
     ax_main.pcolormesh(x, y, z, cmap=cmap)
 
     marginal_x = np.sum(z, axis=1)
-    ax_marginal_x.plot(x[:,0], marginal_x)
+    ax_marginal_x.plot(x[:, 0], marginal_x)
     [l.set_rotation(-90) for l in ax_marginal_y.get_xticklabels()]
 
     marginal_y = np.sum(z, axis=0)
@@ -93,9 +93,9 @@ def Gauss2D(x, y, mu, sigma_pars):
     mu_x, mu_y = mu
     sigma_x, sigma_y, rho = sigma_pars
     A = 1 / (2 * np.pi * 1. * 1. * np.sqrt(1-0.5**2))
-    B = np.exp(-((x - mu_x)**2 / sigma_x**2 + (y - mu_y)**2 / sigma_y**2 - 
-                 2 * rho * (x - mu_x) * (y - mu_y) / sigma_x / sigma_y) / 
-                 (2 * (1 - rho**2)) )
+    B = np.exp(-((x - mu_x)**2 / sigma_x**2 + (y - mu_y)**2 / sigma_y**2 -
+                 2 * rho * (x - mu_x) * (y - mu_y) / sigma_x / sigma_y) /
+                (2 * (1 - rho**2)))
     return A * B
 
 
@@ -106,14 +106,16 @@ def prior(beta, params):
     P = np.exp(S) / (2 * np.pi * sigma0 * sigma1)
     return P
 
+
 def fill_prior(beta0_grid, beta1_grid, prior_params):
     output = np.zeros(beta0_grid.shape)
     ni, nj = beta0_grid.shape
     for i in range(ni):
         for j in range(nj):
-            output[i, j] = prior([beta0_grid[i,j], 
-                                 beta1_grid[i,j]], prior_params)
+            output[i, j] = prior([beta0_grid[i, j],
+                                 beta1_grid[i, j]], prior_params)
     return output
+
 
 def likelihood(beta, data):
     beta0, beta1 = beta
@@ -126,65 +128,60 @@ def likelihood(beta, data):
     L = (2 * np.pi * 1.5**2)**(-N / 2.) * np.exp(-S / 2 / 1.5**2)
     return L
 
+
 def fill_likelihood(beta0_grid, beta1_grid, data):
     output = np.zeros(beta0_grid.shape)
     ni, nj = beta0_grid.shape
     for i in range(ni):
         for j in range(nj):
-            output[i, j] = likelihood([beta0_grid[i,j], beta1_grid[i,j]], data)
+            output[i, j] = likelihood([beta0_grid[i, j],
+                                      beta1_grid[i, j]], data)
     return output
 
 datos = np.loadtxt('espectro.dat')
 
 w = datos[:, 0]
 fnu = datos[:, 1]
-fnu=fnu*(10**18)
+fnu = fnu*(10**19)
 plt.figure(0)
-plt.plot(w,fnu)
+plt.plot(w, fnu)
 plt.xlabel('Longitud de onda w [Angstrom]')
-plt.ylabel('Flujo por unidad de freuencia [erg / s / Hz / cm^2 * 10^18]')
+plt.ylabel('Flujo por unidad de freuencia [erg / s / Hz / cm^2 * 10^19]')
 
-x, y = np.mgrid[6000:7000:50j, 50:200:50j]
-    
-ni, nj = x.shape
-g2d = np.zeros((ni, nj))
-mu = [np.mean(w), np.mean(fnu)]
-sigma_pars = [np.std(w), np.std(fnu), 0]
-for i in range(ni):
-    for j in range(nj):
-        g2d[i,j] = Gauss2D(x[i,0], y[0,j], mu, sigma_pars)
-
-plt.figure(1)
-ax1, ax2, ax3 = plot_distribution(x, y, g2d)
-ax1.axvline(mu[0], lw=6563, color='w')
-ax1.axhline(mu[1], lw=6563, color='w')
-ax2.axvline(mu[0], lw= 100, color='k')
-ax3.axhline(mu[1], lw= 100, color='k')
-
-beta0_grid, beta1_grid = np.mgrid[6000:7000:50j, 50:200:50j]
-beta_prior_pars = [6563, 6460, 100, 91]
+beta0_grid, beta1_grid = np.mgrid[6400:6700:201j, 900:1100:201j]
+beta_prior_pars = [6563, 58, 1000, 14]
 prior_grid = fill_prior(beta0_grid, beta1_grid, beta_prior_pars)
 
-plt.figure(2)
-plt.pcolormesh(beta0_grid, beta1_grid, prior_grid, cmap='viridis')
+plt.figure(1)
+plt.pcolormesh(beta0_grid, beta1_grid, prior_grid, cmap='summer')
 plt.gca().set_aspect('equal')
+plt.xlabel('Longitud de onda w [Angstrom]')
+plt.ylabel('Flujo por unidad de freuencia [erg / s / Hz / cm^2 * 10^19]')
+
 
 likelihood_grid = fill_likelihood(beta0_grid, beta1_grid, [w, fnu])
 post_grid = likelihood_grid * prior_grid
 
-plt.figure(3)
-ax1, ax2, ax3 = plot_distribution(beta0_grid, beta1_grid, post_grid)
-ax1.axvline(2.5, lw=0.5, color='w')
-ax1.axhline(3.4, lw=0.5, color='w')
-ax2.axvline(2.5, lw=1., color='k')
-ax3.axhline(3.4, lw=1., color='k')
+plt.figure(2)
+plt.pcolormesh(beta0_grid, beta1_grid, post_grid, cmap='summer')
+plt.xlabel('Longitud de onda w [Angstrom]')
+plt.ylabel('Flujo por unidad de freuencia [erg / s / Hz / cm^2 * 10^19]')
+ax1, ax2, ax3 = plot_distribution(beta0_grid, beta1_grid,
+                                  post_grid, cmap='summer')
+ax1.axvline(6675, lw=0.5, color='w')
+ax1.axhline(1025, lw=0.5, color='w')
+ax2.axvline(6675, lw=1., color='k')
+ax3.axhline(1025, lw=1., color='k')
 
-beta0_grid, beta1_grid = np.mgrid[-5:11:50j, 1:6:50j]
-dx = 16 / 200
-dy = 5 / 100
+'''
+#beta0_grid, beta1_grid = np.mgrid[-5:11:50j, 1:6:50j]
+#dx = 16 / 200
+#dy = 5 / 100
 
-prior_grid = fill_prior(beta0_grid, beta1_grid, [1, 100, 10, 100])
-likelihood_grid = fill_likelihood(beta0_grid, beta1_grid, [w, fnu])
+##prior_grid = fill_prior(beta0_grid, beta1_grid, [1, 100, 10, 100])
+##likelihood_grid = fill_likelihood(beta0_grid, beta1_grid, [w, fnu])
 
-plt.figure(4)
-plt.plot_distribution(beta0_grid, beta1_grid, likelihood_grid * prior_grid)
+#plt.figure(4)
+#plt.plot_distribution(beta0_grid, beta1_grid, likelihood_grid * prior_grid)
+'''
+
